28c856f7932f4040272bb680854297de2665eaf2
ori0o0p <openwund8517@gmail.com>
2023-07-04 22:05:26 +0900

    add comment

--- src/main/java/com/example/safeteenbackend/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/safeteenbackend/domain/comment/controller/CommentController.java
@@ -1,31 +1,33 @@
 package com.example.safeteenbackend.domain.comment.controller;
 
+import com.example.safeteenbackend.domain.comment.controller.dto.request.CommentRequest;
+import com.example.safeteenbackend.domain.comment.controller.dto.response.CommentResponse;
 import com.example.safeteenbackend.domain.comment.entity.CommentEntity;
 import com.example.safeteenbackend.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class CommentController {
 
-    @Autowired
-    private CommentService cS;
+
+    private final CommentService cS;
 
     // 답변 작성
-    @PostMapping("/writedo")
-    @ResponseBody
-    public CommentEntity writeComment(@RequestBody CommentEntity comment) {
-        cS.write(comment);
-        return comment;
+    @PostMapping("/comment")
+    public CommentResponse writeComment(@RequestBody CommentRequest comment) {
+
+        return cS.write(comment);
     }
 
     // 글 삭제
-    @DeleteMapping("/delete/{id}")
-    public String deleteComment(@PathVariable Integer id) {
+    @DeleteMapping("/comment/{id}")
+    public void deleteComment(@PathVariable Integer id) {
         cS.deleteComment(id);
-        return "삭제";
     }
 }
--- src/main/java/com/example/safeteenbackend/domain/comment/entity/CommentEntity.java
+++ src/main/java/com/example/safeteenbackend/domain/comment/entity/CommentEntity.java
@@ -2,25 +2,29 @@ package com.example.safeteenbackend.domain.comment.entity;
 
 import com.example.safeteenbackend.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Data
+@Builder
 @Entity(name = "comment")
 public class CommentEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)
     private String content;
 
     @Column(nullable = false)
-    private String createCommentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
+    private LocalDateTime createCommentTime;
 
-    @ManyToOne @JoinColumn(name = "user_id")
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
 }
