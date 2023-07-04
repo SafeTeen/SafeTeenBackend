3721de5b5be107af4f56798d6963937cd51d52fd
wheeeung <whee050916@gmail.com>
2023-07-04 14:53:55 +0900

    feat:: setting

---
+++ src/main/java/com/example/safeteenbackend/SafeTeenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.safeteenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeTeenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeTeenBackendApplication.class, args);
+    }
+
+}
