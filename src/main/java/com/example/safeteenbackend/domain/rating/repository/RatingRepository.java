de90d877e64cc508fda92a92ee0250c95377dbc0
wheeeung <whee050916@gmail.com>
2023-07-04 20:55:45 +0900

    feat:: delete rating

--- src/main/java/com/example/safeteenbackend/domain/rating/repository/RatingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.safeteenbackend.domain.rating.repository;
-
-import com.example.safeteenbackend.domain.rating.entity.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, Integer> {
-}
